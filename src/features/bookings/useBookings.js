a1c9a90ec83d993369a621a7505cac316164721f
Paskkal.IO <74718314+Pasify@users.noreply.github.com>
2024-12-24 19:59:12 +0100

    FEAT: implemet server side filtering on the bookings table

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,15 +1,25 @@
 import { useQuery } from "@tanstack/react-query";
 
 import { getBookings } from "../../services/apiBookings";
+import { useSearchParams } from "react-router-dom";
 
 export function useBookings() {
+  const [searchParam] = useSearchParams();
+  searchParam.get("");
+  // filtering logic
+  const filteredValue = searchParam.get("status");
+  const filter =
+    !filteredValue || filteredValue === "all"
+      ? null
+      : { field: "status", value: filteredValue };
+  // { field: "totalPrice", value: 5000, method: "gte" };
   const {
     isLoading,
     data: bookings,
     error,
   } = useQuery({
-    queryKey: ["bookings"],
-    queryFn: getBookings,
+    queryKey: ["bookings", filter],
+    queryFn: () => getBookings({ filter }),
   });
   return { isLoading, bookings, error };
 }
